eda8d66cbd5433818596097120ec58f77eed6b46
Abhinav Jha <abhi28.jha@gmail.com>
2021-05-05 23:34:32 +0530

    Learning Spring Security

---
+++ Learn-SpringSecurity/src/test/java/com/abhinavjdwij/learn/LearnSpringSecurity/LearnSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinavjdwij.learn.LearnSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
