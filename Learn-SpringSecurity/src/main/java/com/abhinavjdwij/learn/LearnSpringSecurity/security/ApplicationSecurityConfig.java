a701aa5617a15e7fc41ed8d32b653422dd018393
Abhinav Jha <abhi28.jha@gmail.com>
2021-05-06 21:50:06 +0530

    Learning Spring Security

---
+++ Learn-SpringSecurity/src/main/java/com/abhinavjdwij/learn/LearnSpringSecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.abhinavjdwij.learn.LearnSpringSecurity.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/*
+Basic Auth example
+Drawback: Basic Auth requires username / password for each request, no login / logout feature
+ */
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
