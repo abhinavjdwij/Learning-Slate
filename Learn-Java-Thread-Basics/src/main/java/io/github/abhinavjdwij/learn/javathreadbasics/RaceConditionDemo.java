75f46fb47233215bb6ec5b18121d95c17d5410ee
Abhinav Jha <abhi28.jha@gmail.com>
2021-05-23 00:59:33 +0530

    Learn Java Thread Basics

--- Learn-Java-Thread-Basics/src/main/java/io/github/abhinavjdwij/learn/javathreadbasics/RaceConditionDemo.java
+++ Learn-Java-Thread-Basics/src/main/java/io/github/abhinavjdwij/learn/javathreadbasics/RaceConditionDemo.java
@@ -1,7 +1,25 @@
 package io.github.abhinavjdwij.learn.javathreadbasics;
 
 public class RaceConditionDemo {
-    public static void main(String[] args) {
-        
+    private static int val = 100;
+
+    public static void main(String[] args) throws Exception {
+
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " executing ");
+            if (val == 100) {
+                val /= 5;
+            }
+        }, "By-5-Thread");
+        Thread t2 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " executing ");
+            val = 25;
+        }, "To-25-Thread");
+        t1.start();
+        t2.start();
+        // t1 and t2 will have race condition, final value of val is non-deterministic
+        Thread.sleep(1000); // allow some time for evaluation
+        System.out.println("Final value of val :: " + val);
+
     }
 }
---
+++ Learn-Java-Thread-Basics/src/main/java/io/github/abhinavjdwij/learn/javathreadbasics/SynchronizationDemo.java
@@ -0,0 +1,30 @@
+package io.github.abhinavjdwij.learn.javathreadbasics;
+
+import java.util.List;
+
+public class SynchronizationDemo {
+    private static int counter = 1; // the variable can also be synchronized (create class and use synchronized variable and method)
+
+    // if not synchronized, this method will return ambiguous output in concurrent environment
+    private static synchronized int incrementAndGetId(){
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException interruptedException) {
+            interruptedException.printStackTrace();
+        }
+        return counter++;
+    }
+
+    public static void main(String[] args) {
+        Runnable r = () -> System.out.println("Counter :: " + incrementAndGetId()
+                + " Thread name :: " + Thread.currentThread().getName());
+        List<Thread> threads = List.of(
+                new Thread(r, "T1"),
+                new Thread(r, "T2"),
+                new Thread(r, "T3"),
+                new Thread(r, "T4"),
+                new Thread(r, "T5")
+        );
+        threads.forEach(Thread::start);
+    }
+}
