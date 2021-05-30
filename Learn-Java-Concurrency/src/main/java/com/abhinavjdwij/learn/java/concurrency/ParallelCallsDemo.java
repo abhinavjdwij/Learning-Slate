a05a8d192336e2c2cf7058a117678b8d9221015a
Abhinav Jha <abhi28.jha@gmail.com>
2021-05-31 00:50:50 +0530

    Java Multithreading practice

--- Learn-Java-Concurrency/src/main/java/com/abhinavjdwij/learn/java/concurrency/ParallelCallsDemo.java
+++ Learn-Java-Concurrency/src/main/java/com/abhinavjdwij/learn/java/concurrency/ParallelCallsDemo.java
@@ -5,24 +5,49 @@ package com.abhinavjdwij.learn.java.concurrency;
 */
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ParallelCallsDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         List<Integer> delays = List.of(1000, 2000, 3000, 4000, 5000);
 
         // Using simple Threads -> return value not available in main method
-        System.out.println("Using simple Threads");
-        delays.forEach((delay) -> {
-            Thread t = new Thread(() -> new MockClient().mockExternalCall(delay));
-            t.start();
-        });
-        System.out.println();
+//        System.out.println("Using simple Threads :: ");
+//        delays.forEach((delay) -> {
+//            Thread t = new Thread(() -> new MockClient().mockExternalCall(delay));
+//            t.start();
+//        });
+//        System.out.println();
 
-        // Using parallel stream
-        System.out.println("Using parallel stream :: ");
-        delays.parallelStream().map(delay -> new MockClient().mockExternalCall(delay)).forEach(System.out::println);
-        System.out.println();
+//         Using parallel stream
+//        System.out.println("Using parallel stream :: ");
+//        delays.parallelStream().map(delay -> new MockClient().mockExternalCall(delay)).forEach(System.out::println);
+//        System.out.println();
+
+        // Using Completable Future
+        System.out.println("Using Completable Future :: ");
+        Stream<CompletableFuture<String>> futures = delays
+                .stream()
+                .map(delay -> CompletableFuture.supplyAsync(() -> new MockClient().mockExternalCall(delay)));
+
+        System.out.println("Waiting for all processes to complete");
+        while (futures.map(future -> future.isDone()).anyMatch(done -> !done)) {
+            // Can use timeout using System time here if needed
+        }
+        System.out.println("All futures completed");
+        futures.forEach(future -> {
+            try {
+                System.out.println(future.get());
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
 
     }
 
